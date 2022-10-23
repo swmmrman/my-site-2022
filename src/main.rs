fdb5880c820a9ced14f2d560f6e1edd54da967de
Stephan Kern <stephan@swmmrman.com>
2022-10-23 00:08:33 -0600

    Change cargo to rc version and start rocket setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+async fn index() -> Option<rocket::fs::NamedFile>{
+    rocket::fs::NamedFile::open(Path::new("public_html/index.html")).await.ok()
+}
+
+#[launch]
+async fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
 }
