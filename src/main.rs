0d6ef5e381e17025aaa895a56702055d1573c30f
Stephan Kern <stephan@swmmrman.com>
2022-10-23 00:13:05 -0600

    Css Method

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,13 @@ use std::path::Path;
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-async fn index() -> Option<rocket::fs::NamedFile>{
+async fn index() -> Option<rocket::fs::NamedFile> {
     rocket::fs::NamedFile::open(Path::new("public_html/index.html")).await.ok()
+}
+
+#[get("/css/<file_name>")]
+async fn css(file_name: &str) -> Option<rocket::fs::NamedFile> {
+    rocket::fs::NamedFile::open(Path::new("css/").join(file_name)).await.ok()
 }
 
 #[launch]
