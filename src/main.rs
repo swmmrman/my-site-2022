cd152e28a5413487046a226e1cf90e7d2e029705
Stephan Kern <stephan@swmmrman.com>
2022-11-07 15:18:41 -0700

    Remove JS method

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,9 @@ async fn css(file_name: &str) -> Option<rocket::fs::NamedFile> {
     rocket::fs::NamedFile::open(Path::new("public_html/css").join(file_name)).await.ok()
 }
 
-#[get("/js/<file_name>")]
-async fn js(file_name: &str) -> Option<rocket::fs::NamedFile> {
-    rocket::fs::NamedFile::open(Path::new("public_html/js").join(file_name)).await.ok()
-}
-
 #[launch]
 async fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index, css])
-        .mount("/js/", FileServer::from("public_html/js/"))
-        
+        .mount("/js/", FileServer::from("public_html/js/"))        
 }
