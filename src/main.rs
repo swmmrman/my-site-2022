db55729473cd38df2244a1fd98e21551fd461e80
Stephan Kern <stephan@swmmrman.com>
2022-11-07 15:20:14 -0700

    Move CSS to fileserver and remove route/function

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,10 @@ async fn index() -> Option<rocket::fs::NamedFile> {
     rocket::fs::NamedFile::open(Path::new("public_html/index.html")).await.ok()
 }
 
-#[get("/css/<file_name>")]
-async fn css(file_name: &str) -> Option<rocket::fs::NamedFile> {
-    rocket::fs::NamedFile::open(Path::new("public_html/css").join(file_name)).await.ok()
-}
-
 #[launch]
 async fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, css])
-        .mount("/js/", FileServer::from("public_html/js/"))        
+        .mount("/", routes![index])
+        .mount("/js/", FileServer::from("public_html/js/"))
+        .mount("/css/", FileServer::from("public_html/css/"))
 }
