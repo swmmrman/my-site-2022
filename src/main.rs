3ac1e0d1bc7a12d0934ae3e45adf00f0f59d12f7
Stephan Kern <stephan@swmmrman.com>
2022-11-08 17:50:37 -0700

    Make it html, probably the wrong way

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::path::Path;
-use rocket::fs::FileServer;
+use rocket::{fs::FileServer, response::content::RawHtml};
 #[macro_use] extern crate rocket;
 
 #[get("/")]
@@ -7,13 +7,13 @@ async fn index() -> Option<rocket::fs::NamedFile> {
     rocket::fs::NamedFile::open(Path::new("public_html/index.html")).await.ok()
 }
 #[post("/post.html", data = "<text_field>")]
-async fn post(text_field: &str) -> String {
+async fn post(text_field: &str) -> RawHtml<String> {
     let text_fields: Vec<&str> = text_field.split("&").collect();
     let field_0: &str = text_fields[0].split("=").collect::<Vec<&str>>()[1];
     let field_1: &str = text_fields[1].split("=").collect::<Vec<&str>>()[0];
-    format!("{}\n{}",
-        field_0,
-        field_1
+    let text = format!(r#"<html>{}\<br>{}</html>"#, field_0, field_1).to_owned();
+    RawHtml(
+        text
     )
 }
 
