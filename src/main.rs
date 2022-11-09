95e8d32cd0ad0474c197b790bb2f5db150e7f6b9
Stephan Kern <stephan@swmmrman.com>
2022-11-08 17:01:52 -0700

    testing post method

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,15 @@ use rocket::fs::FileServer;
 async fn index() -> Option<rocket::fs::NamedFile> {
     rocket::fs::NamedFile::open(Path::new("public_html/index.html")).await.ok()
 }
+#[post("/post.html", data = "<text_field>")]
+async fn post(text_field: &str) -> &str {
+    text_field.clone()
+}
 
 #[launch]
 async fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, post])
         .mount("/js/", FileServer::from("public_html/js/"))
         .mount("/css/", FileServer::from("public_html/css/"))
 }
